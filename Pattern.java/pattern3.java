5d131d7f898c658966fb426bcc75190c3dd3acf3
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-25 01:30:22 +0530

    .

---
+++ Pattern.java/pattern3.java
@@ -0,0 +1,11 @@
+
+public class pattern3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j=0;j<=i;j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}

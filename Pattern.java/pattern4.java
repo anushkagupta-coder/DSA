fafc5304137b66b437982bed176e64c946c01101
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-25 01:35:32 +0530

    ,

--- Pattern.java/pattern4.java
+++ Pattern.java/pattern4.java
@@ -1,12 +1,11 @@
 
-
-
 public class pattern4 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for(int j=0;j<=i;j++){
-                System.out.print(i);
+        for (int i = 1; i <= 4; i++) {
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
             }
+            System.out.println();
         }
         System.out.println();
     }

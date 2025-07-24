e2883c27690e66993c3593239365836d58d5f0c4
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-25 01:44:48 +0530

    .

---
+++ Pattern.java/pattern5.java
@@ -0,0 +1,17 @@
+
+
+public class pattern5 {
+    public static void main(String[] args) {
+        for (int i = 4; i <=0 ; i++) {
+            for (int j = i; j <i+1; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+/* 
+...
+.. 
+.
+*/ 

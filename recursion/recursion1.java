fcb78dc5aa6229cf1f2df652fafd173c58186338
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-25 18:39:12 +0530

    Update recursion1.java

--- recursion/recursion1.java
+++ recursion/recursion1.java
@@ -1,6 +1,13 @@
 //to print name 5 times using recursion
 public class recursion1 {
     public static void main(String[] args) {
-        
+        int n=5;
+        f(i,n);
     }
+    public void f(i,n){
+            if(i>n)
+            return;
+            System.out.println("ANUSHKA");
+            f(i+1,n);
+        }
 }

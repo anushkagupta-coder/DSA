b0473d627f29036e3cf05c03d8671e3231bcdebf
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-27 20:22:44 +0530

    selection sort

--- Sorting/selectionsort.java
+++ Sorting/selectionsort.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class selectionsort {
     public static void main(String[] args) {
         
-        Scanner scanner = new scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter size of array: ");
         int n = scanner.nextInt();
@@ -14,9 +14,26 @@ public class selectionsort {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         
-        selection(int arr[],n);
+        selection( arr,n);
+        System.out.println("sorted array is:");
+        for(int k=0;k<n;k++){
+            System.out.println(arr[i]);
+        }
     }
-    static void selection(){
-        
+    
+}
+    static void selection(int[] arr,int n){
+        for(int i=0;i<n-2;i++){
+            int mini=i;
+            for(int j=i;j<n-1;j++){
+                if(arr[j]<arr[i]){
+                    mini=j;
+                }
+            }
+            int temp=arr[mini];
+            arr[mini]=arr[i];
+            arr[i]=temp;
+        }
     }
 }
+

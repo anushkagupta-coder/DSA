2ca4b83121bdbebaa4d69d71792c9598b653b404
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-29 23:34:53 +0530

    remove duplicate values

---
+++ Sorting/removeDuplicates.java
@@ -0,0 +1,18 @@
+
+
+public class removeDuplicates {
+    public static void main(String[] args) {
+        int[] arr={2,5,2,6,6,6,4};
+        removeduplicate(arr,7);
+    }
+    static int removeduplicate(int[] arr,int n){
+        int i=0;
+        for(int j=0;j<n;j++){
+            if(arr[j]!=arr[j]){
+                arr[j+1]=arr[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+}

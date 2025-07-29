134f9aa050b4bbb98d43b846efd77fd7e166b8a0
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-29 23:39:46 +0530

    Update removeDuplicates.java

--- Sorting/removeDuplicates.java
+++ Sorting/removeDuplicates.java
@@ -2,14 +2,15 @@
 
 public class removeDuplicates {
     public static void main(String[] args) {
+        int n=7;
         int[] arr={2,5,2,6,6,6,4};
         removeduplicate(arr,7);
     }
     static int removeduplicate(int[] arr,int n){
         int i=0;
         for(int j=0;j<n;j++){
-            if(arr[j]!=arr[j]){
-                arr[j+1]=arr[j];
+            if(arr[i]!=arr[j]){
+                arr[i+1]=arr[j];
                 i++;
             }
         }

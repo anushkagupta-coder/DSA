be870b887d58957bac935533e30dcd15b31f2db2
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-29 16:15:42 +0530

    check if array is sorted

---
+++ Sorting/checksorted.java
@@ -0,0 +1,14 @@
+public class checksorted {
+    public static void main(String[] args) {
+        
+    }
+    static int checksorted(int[] arr,int n){
+        if (int i=1;i<n;i++) {
+            
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}

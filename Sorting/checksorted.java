ccd9696898e495588f01435cc6fa0ca3ceb177ca
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-29 16:20:31 +0530

    .

--- Sorting/checksorted.java
+++ Sorting/checksorted.java
@@ -1,14 +1,21 @@
 public class checksorted {
     public static void main(String[] args) {
-        
+        int[] arr={1,2,3,4};
+        int n=arr.length;
+        checksorted(arr,n);
+        System.out.println("checked");
     }
-    static int checksorted(int[] arr,int n){
-        if (int i=1;i<n;i++) {
-            
-        }
-        else{
-            return false;
-        }
+    static boolean checksorted(int[] arr,int n)
+    {
+        for(int i=1;i<n;i++) {
+            if(arr[i]>=arr[i-1]){
+
+            }
+            else{
+                return false;
+            }
     }
     return true;
+}
+    
 }

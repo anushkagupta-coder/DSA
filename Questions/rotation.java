3a3145d845857a5933ee1836e959bc375a4da9d2
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-30 00:16:39 +0530

    Update rotation.java

--- Questions/rotation.java
+++ Questions/rotation.java
@@ -1,4 +1,4 @@
-//THIS PROGRAM IS TO ROTATE THE ARRAY LEFT
+//THIS PROGRAM IS TO ROTATE THE ARRAY LEFT by one
 public class rotation {
     public static void main(String[] args){
         int[] arr={1,2,3,4,5};

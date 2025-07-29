d9f646f70f82f0f5232e10abdd6fc74af516358c
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-29 15:33:05 +0530

    ,

--- Sorting/largest.java
+++ Sorting/largest.java
@@ -1,6 +1,7 @@
 
 import java.util.Scanner;
 public class largest {
+        
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter no of elements");
@@ -8,21 +9,22 @@ public class largest {
 
         int[] arr=new int[n];
 
-        System.out.println("enter elements of array");
-        for(int i=0 ; i <  n;i++){
+        System.out.print("enter elements of array");
+        for(int i=0;i<n;i++){
             arr[i]=scanner.nextInt();
         }
-        largestelment(arr);
+        largestelement(arr,n);
         
     }
-    static void largestelment(int[] arr){
+    static void largestelement(int[] arr,int n){
         int largest=arr[0];
+        
         for(int i=0;i<n;i++){
             if (arr[i]>largest){
                 largest=arr[i];
             }
-            System.out.println(largest+"is the largest element");
-        
+            
         }
+        System.out.println(largest+"is the largest element");
     }
 }

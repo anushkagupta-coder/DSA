0b08857cb518b0520f9b4eb2be18141bee2f576d
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-29 15:21:10 +0530

    laregst element

---
+++ Sorting/largest.java
@@ -0,0 +1,15 @@
+impoet java.util.Scanner;
+public class largest {
+    public static void main(String[] args) {
+        System.out.println("enter no of elements");
+    }
+    static void largest(int[] arr){
+        largest=arr[0];
+        for()[
+            if (arr[i]>largest){
+                largest=arr[i];
+            }
+            System.out.println(largest+"is the largest element");
+        ]
+    }
+}

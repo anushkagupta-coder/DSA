6a036052b94c4ecad4e20f998b5e2b38d55efdea
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-07-31 00:32:38 +0530

    missing number

---
+++ Questions/missingnum.java
@@ -0,0 +1,9 @@
+//THIS PROGRAM IS TO FIND THE MISSING NUMBER;
+public class missingnum {
+    public static void main(Stringp[ args]){
+
+    }
+    static void missing(int[] arr,int n){
+
+    }
+}

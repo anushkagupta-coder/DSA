053796c8c7ddfc02c1234b5a5941eb6d274de30a
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-02 23:02:07 +0530

    Update recursion2.java

--- recursion/recursion2.java
+++ recursion/recursion2.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-// to print from 1-N using recusrion
-
+// to print from 1-N USING RECURSION
 public class recursion2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

7bc109a7af3d1cada9148e27c9033c28927e32fe
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-21 23:10:20 +0530

    Update 2timesand1time.java

--- Questions/2timesand1time.java
+++ Questions/2timesand1time.java
@@ -1,8 +1,12 @@
 
 //this code is to find element which will occur only once while all other twice
 public class 2timesand1time {
-    class Solution {
-    public int singleNumber(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums={1,1,2,3,3,4,4};
+        singleNumber(nums);
+    }
+    
+    static int singleNumber(int[] nums) {
         int xorr=0;
         for(int i=0;i<nums.length;i++){
             xorr=xorr ^ nums[i] ;
@@ -10,4 +14,4 @@ public class 2timesand1time {
         return xorr ;
     }
 }
-}
+

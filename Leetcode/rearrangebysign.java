b59357d328c9b76ed199d2de23718ab5fbe42d2e
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-24 18:18:03 +0530

    Update rearrangebysign.java

--- Leetcode/rearrangebysign.java
+++ Leetcode/rearrangebysign.java
@@ -8,6 +8,9 @@ public class rearrangebysign {
 
     }
     static int rearrange(int[] arr){
+        int[] ans =new int[arr.length];
+        int posIndex=0;
+        int negIndex=1;
         for(int i=0;i<arr.length;i++){
             if (arr[i]<0){
                 ans[negIndex]=arr[i];

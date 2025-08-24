0ff293be5ef01892f0a90dcc481de780bf502e86
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-24 17:51:27 +0530

    rearrange alternate by signs

---
+++ Leetcode/rearrangebysign.java
@@ -0,0 +1,14 @@
+
+
+public class rearrangebysign {
+    public static void main(String[] args) {
+        int[] arr={1,-2,-3,2,3,-1};
+        
+        System.out.println(rearrange(arr));
+    }
+    static void rearrange(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            if arr[i]<0
+        }
+    }
+}

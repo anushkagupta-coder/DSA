b3112f03d43149a15bcc96a82f58fb06cb9e23b2
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-27 09:48:23 +0530

    Update stock.java

--- Leetcode/stock.java
+++ Leetcode/stock.java
@@ -1,6 +1,6 @@
 public class stock {
     public static void main(String[] args) {
-        int[] prices={1,7,4,3,5};
+        int[] prices={7,1,5,3,6,4};
         System.out.println(stock(prices));
     }
     static int stock(int[] prices){

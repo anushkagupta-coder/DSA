2182cfac3830909bc441d35c321691c5c525e812
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-27 10:36:33 +0530

    Update leaders_in_array.java

--- Leetcode/leaders_in_array.java
+++ Leetcode/leaders_in_array.java
@@ -3,6 +3,18 @@ public class leaders_in_array {
     public static void main(String[] args) {
         int[] arr={16,17,4,3,5,2}
         System.out.println("(the leaders of array are)");
-        System.out.println(leaders(arr[]));
+        (leaders(arr[]));
+    }
+    static void leaders(int[] arr){
+        int n=arr.length;
+        int maxfromright=arr[n-1];
+        System.out.println(maxfromrigth+" ");
+
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>=maxfromright){
+                maxfromright=arr[i];
+                System.out.println(maxfromright+" ");
+            }
+        }
     }
 }

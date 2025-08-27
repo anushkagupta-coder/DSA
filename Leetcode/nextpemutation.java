1d61ca01148a4e5cc329823e12db2734a3e077e8
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-27 10:19:22 +0530

    next permutation

---
+++ Leetcode/nextpemutation.java
@@ -0,0 +1,37 @@
+public class nextpemutation {
+    class Solution {
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int index=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1){
+            reverse(nums,0,n);
+            return ;
+        }
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[index]){
+                int temp=nums[i];
+                nums[i]=nums[index];
+                nums[index]=temp;
+                break;
+            }
+        }
+        reverse(nums,index+1,n);
+    }
+    private static void reverse(int[] nums, int start, int end){
+        end--;
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+}

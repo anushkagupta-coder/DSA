6282781fa3cbb9d39fa5ae253075fed978569ce7
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-27 09:47:32 +0530

    stocks problem

---
+++ Leetcode/stock.java
@@ -0,0 +1,19 @@
+public class stock {
+    public static void main(String[] args) {
+        int[] prices={1,7,4,3,5};
+        System.out.println(stock(prices));
+    }
+    static int stock(int[] prices){
+        int buy=prices[0];
+        int profit=0;
+        int n=prices.length;
+        for(int i=0;i<n;i++){
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
+    }
+}

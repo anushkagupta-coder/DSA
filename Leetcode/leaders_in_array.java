5da5eedb3598445b49fab4e5b426b6e42c7be28c
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-27 10:34:04 +0530

    Update leaders_in_array.java

--- Leetcode/leaders_in_array.java
+++ Leetcode/leaders_in_array.java
@@ -1,6 +1,8 @@
 //everthing to the right side of the number must br smaller and that number is callled as leader;
 public class leaders_in_array {
     public static void main(String[] args) {
-        
+        int[] arr={16,17,4,3,5,2}
+        System.out.println("(the leaders of array are)");
+        System.out.println(leaders(arr[]));
     }
 }

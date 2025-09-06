d26684cb1322f12269ca4f8f5ba3a814cd52ebfb
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-06 17:05:55 +0530

    Update matrixx_0.java

--- Leetcode/matrixx_0.java
+++ Leetcode/matrixx_0.java
@@ -1,1 +1,48 @@
 //in this program to make row and col element 0 when 0 is encounterd
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        // Use a variable to track if the first column needs to be zeroed
+        int col0 = 1;
+        
+        // First pass: Use the first row and column as markers
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(matrix[i][j] == 0){
+                    // Mark the row
+                    matrix[i][0] = 0;
+                    // Mark the column. Handle the first column separately.
+                    if(j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+        
+        // Second pass: Use markers to set the inner matrix to zero
+        for(int i = 1; i < n ; i++){
+            for(int j = 1 ; j < m ; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Final pass: Zero out the first row and/or column if needed
+        if(matrix[0][0] == 0) {
+            for(int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(col0 == 0) {
+            for(int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}

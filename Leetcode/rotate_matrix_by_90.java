347d10ed363a2d212e1caa0b2fb1078430782070
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-07 12:45:29 +0530

    Update rotate_matrix_by_90.java

--- Leetcode/rotate_matrix_by_90.java
+++ Leetcode/rotate_matrix_by_90.java
@@ -3,6 +3,11 @@ public class rotate_matrix_by_90 {
         
     }
     static void rotate(int[][] matrix){
-        
+        int n=matrix.length;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-2;i++){
+                matrix[i][j]=matrix[j][i];
+            }
+        }
     }
 }

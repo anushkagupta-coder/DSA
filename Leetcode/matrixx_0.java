86a92a6b12c5bd98a361edc78dc9a802f84d2a0b
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-07 12:22:17 +0530

    Update matrixx_0.java

--- Leetcode/matrixx_0.java
+++ Leetcode/matrixx_0.java
@@ -59,7 +59,34 @@ public class matrixx_0{
         int col0=1;
         for (int i = 0; i < n; i++){
             for(int j=0; j<n; j++){
-                
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0 ){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0=0;
+                    }
+                }
+            }
+        }
+        //second paas 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(matrix[i][0]==0 || matrix[0][j]==00){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        //final pass 
+        if(matrix[0][0]==0){
+            for(int j=0;j<m;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0;i<n; i++){
+                matrix[i][0]=0;
             }
         }
     }

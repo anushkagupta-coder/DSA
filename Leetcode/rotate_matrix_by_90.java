1a809d28375b9752e3416b1b9c75275595b22213
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-07 13:04:31 +0530

    Update rotate_matrix_by_90.java

--- Leetcode/rotate_matrix_by_90.java
+++ Leetcode/rotate_matrix_by_90.java
@@ -1,10 +1,14 @@
 //this program is ti rotate the matrix by 90 degree;
 public class rotate_matrix_by_90 {
     public static void main(String[] args) {
-        int[][] matrix={{1,2,3},
-                    {4,5,6},
-                    {7,8,9}};
-        System.out.println(rotate(matrix));
+        int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
+        int[][] rotated = rotate(matrix);
+
+        for(int i=0;i<rotated.length;i++){
+            for(int j=0;j<roatted.length;j++){
+
+            }
+        }
         
     }
     static int[][] rotate(int[][] matrix){
@@ -22,9 +26,9 @@ public class rotate_matrix_by_90 {
         for(int i=0;i<n;i++){
             for(int j=0;j<n/2;j++){
                 int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][n-1-j];
-                matrix[j][n-1-j]=temp;
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp;
             }
-        }
+        } return matrix;
     }
 }

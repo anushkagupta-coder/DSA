06e81a77f5436e9ccb0a1e56dadac05614208282
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-09 00:33:46 +0530

    Update sum_of_3_google.java

--- Leetcode/sum_of_3_google.java
+++ Leetcode/sum_of_3_google.java
@@ -1,7 +1,8 @@
 //google leetcode question ..this program will show how many 3 numbers will have addition 0;
 public class sum_of_3_google {
     public static void main(String[] args) {
-        
+        List<Integer> nums = new ArrayList<>();
+        System.out.println(threesum(nums));
     }
     static List<List<Integer>> threesum(int[] nums){
         Arrays.sort(nums);
@@ -16,9 +17,19 @@ public class sum_of_3_google {
                 int sum=nums[i]+nums[j]+nums[k];
 
                 if(sum==0){
-                    ans.add(Arrays.asList(nums[i],nu))
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+
+                    while(j<k && nums[k]==nums[k-1]){
+                        k--;
+                    }
+                    j++;
+                    k--;
+
+                }else {
+                    k--;
                 }
             }
         }
+        return ans;
     }
 }

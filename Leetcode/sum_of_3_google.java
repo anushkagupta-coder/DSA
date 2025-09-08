3a0b51a91f2492f0e3f109a6eeb55d47c169fd24
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-09 00:21:09 +0530

    Update sum_of_3_google.java

--- Leetcode/sum_of_3_google.java
+++ Leetcode/sum_of_3_google.java
@@ -1,4 +1,4 @@
-//google leetcode question ..this program will show 
+//google leetcode question ..this program will show how many 3 numbers will have addition 0;
 public class sum_of_3_google {
     
 }

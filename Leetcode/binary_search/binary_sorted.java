6ff74ae5f13d4bd2ad28da9914c52881709603a4
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-10 12:06:10 +0530

    Create binary_sorted.java

---
+++ Leetcode/binary_search/binary_sorted.java
@@ -0,0 +1,23 @@
+package binary_search;
+//this program uses binary search to find X from sorted ;
+
+public class binary_sorted {
+    public static void main(String[] args){
+
+    }   
+    //iterative
+    static int binary(int[] arr,target){
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+        int mid=(low+high)/2;
+        if (mid==target){
+            return mid;
+        }
+        else if (mid>target){
+            low=;
+        }    
+    }}
+    //recursive func 
+}

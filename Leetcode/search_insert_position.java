28e94653aa321bea2c8e108400c3e28e97f6956c
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-12 18:23:10 +0530

    Update search_insert_position.java

--- Leetcode/search_insert_position.java
+++ Leetcode/search_insert_position.java
@@ -1,6 +1,9 @@
 public class search_insert_position {
     public static void main(String[] args) {
-        
+        int arr[] ={1,2,3,6,15,34,39,80};
+        int target=50;
+        int n=arr.length;
+        System.out.println(fun(arr,target,8));
     }
     static int fun(int arr[],int target,int n){
         int low =0;

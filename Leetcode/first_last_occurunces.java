44eb0283e6e69cd3cb43bc482b1dabfa91e852a0
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-12 18:44:23 +0530

    first and last occurence of element

---
+++ Leetcode/first_last_occurunces.java
@@ -0,0 +1,9 @@
+//first and last occurunces in array;
+//this code gives us the index when element occured first time and when element occured last time iin the array;
+public class first_last_occurunces {
+    public static void main(String[] args) {
+        int[] arr={};
+        int n=arr.length;
+    }
+    static int func(int[] arr,int n)
+}

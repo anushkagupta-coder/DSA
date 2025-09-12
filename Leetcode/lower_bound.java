98314fc3abcbc68395ac0604bf331edc9e5db4dd
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-12 18:13:26 +0530

    Update lower_bound.java

--- Leetcode/lower_bound.java
+++ Leetcode/lower_bound.java
@@ -3,22 +3,24 @@ public class lower_bound {
     public static void main(String[] args) {
         int arr[] ={1,2,3,6,15};
         int target=7;
+        int n=arr.length;
         System.out.println(fun(arr,target,5));
     }
     static int fun(int arr[],int target,int n){
         int low =0;
         int high=n-1;
-        int n=arr.length;
-        int ans=n;
+    
         while(low<=high){
             int mid=(low+high)/2;
-            if(arr[mid]>=target){
-                ans=mid;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>target){
                 high=mid-1;
             }
             else 
             low=mid+1;
         }
-        return ans;
+        return target;
     }
 }

9cc78063b899bfe40414db3434d5577c44a442bd
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-12 18:40:20 +0530

    Update floor_ceil.java

--- Leetcode/floor_ceil.java
+++ Leetcode/floor_ceil.java
@@ -6,9 +6,25 @@ public class floor_ceil {
         int[] arr={23,34,39,45,56,67,70};
         int n=arr.length;
         int x=46;
-        System.out.println(floor_ceil(arr,x,n));
+        System.out.println(floor(arr,x,n));
+        System.out.println(ceil(arr,x.n));
     }
-    static int floor_ceil(int[] arr,int x,int n){
+    static int floor(int[] arr,int x,int n){
+        int ans=-1;
+        int low=0;
+        int high=n-1;
+        while (low<=high) { 
+            int mid=(low+high)/2;
+            if(arr[mid]<=x){
+                ans=arr[mid];
+                low=mid+1;
+            }
+            else 
+            high=mid-1;
+        }
+        return ans;
+    }
+    static int ceil(int[] arr,int x,int n){
         int ans=-1;
         int low=0;
         int high=n-1;

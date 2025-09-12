3984041c5b94f40379fec7cfb917c2535bb6e19c
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-12 18:34:00 +0530

    Update floor_ceil.java

--- Leetcode/floor_ceil.java
+++ Leetcode/floor_ceil.java
@@ -2,5 +2,13 @@
 floor=largest no in array <= x;
 ceil= smallest no in array >= x*/
 public class floor_ceil {
-    
+    public static void main(String[] args) {
+        int[] arr={23,34,39,45,56,67,70}
+        int n=arr.length;
+        int x=46;
+        System.out.println(floor_ceil(arr,x));
+    }
+    static int floor_ceil(int[] arr,int x){
+
+    }
 }

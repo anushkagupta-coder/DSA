454ae73651d3a9de14d36dcb8719dc452b175e30
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-12 18:11:05 +0530

    Update lower_bound.java

--- Leetcode/lower_bound.java
+++ Leetcode/lower_bound.java
@@ -1,16 +1,17 @@
 //program used to find lower bound (smallest index such that arr[index]>=x)
 public class lower_bound {
     public static void main(String[] args) {
-        int arr[] ={1,2,3,6,15}
+        int arr[] ={1,2,3,6,15};
         int target=7;
-        System.out.println(fun(arr,target,n));
+        System.out.println(fun(arr,target,5));
     }
     static int[] fun(int arr[],int target,int n){
         int low =0;
         int high=n-1;
-        int n;
+        int n=arr.length;
         int ans=n;
         while(low<=high){
+            int mid=(low+high)/2;
             if(arr[mid]>=target){
                 ans=mid;
                 high=mid-1;

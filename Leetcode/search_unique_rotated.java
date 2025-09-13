74097e64022f4dd163ee7d1bc4ab892a050ecacc
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-13 19:17:05 +0530

    Update search_unique_rotated.java

--- Leetcode/search_unique_rotated.java
+++ Leetcode/search_unique_rotated.java
@@ -4,37 +4,42 @@ public class search_unique_rotated {
         int[] arr={8,9,10,1,2,3,3,3,5,6,7};
         int n=arr.length;
         int target=3;
+        System.out.println(binary(arr,target,n));
     }
-    static int binary(int[] arr,int target,int n){
+    static boolean binary(int[] arr,int target,int n){
         int low=0;
         int high=n-1;
         while(low<=high){
             int mid=(low+high)/2;
             if(arr[mid]==target){
+                return true;
+            }
+            if(arr[low]==arr[mid]){
                 low++;
                 continue;
             }
-            if(arr[mid]==high){
+            if(arr[mid]==arr[high]){
                 high--;
                 continue;
             }
-            if(arr[low]<=target && target<arr[high]) {
-                if (true) {
-                    
+            if(arr[low]<=arr[high]) {
+                if (arr[low]<=target && target<arr[mid]) {
+                    high=mid-1;
                 }
-                else(){
-
+                else{
+                    low=mid+1;
                 }
             }
             else{
-                if (true) {
-                    
+                if (arr[mid]<target && target<=arr[high]) {
+                    low=mid+1;
                 }
                 else{
-
+                    high=mid-1;
                 }
             }
         }
+        return false;
 
     }
 }

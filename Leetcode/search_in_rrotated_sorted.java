f7c2156f48589783dded2e77a66f0cfe840140c0
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-13 17:24:07 +0530

    Update search_in_rrotated_sorted.java

--- Leetcode/search_in_rrotated_sorted.java
+++ Leetcode/search_in_rrotated_sorted.java
@@ -15,11 +15,11 @@ public class search_in_rrotated_sorted {
         
         while(low<=high){
             int mid=(low+high)/2;
-        if(nums[mid]==target){
+        if(arr[mid]==target){
                 return mid;
             }
-        if (nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<=nums[mid]){
+        if (arr[low]<=arr[mid]){
+                if(arr[low]<=target && target<=arr[mid]){
                     high=mid-1;
                 }
                 else{
@@ -27,7 +27,7 @@ public class search_in_rrotated_sorted {
             }
         }
         else{
-                if (nums[mid]<=target && target<=nums[high]){
+                if (arr[mid]<=target && target<=arr[high]){
                     low=mid+1;
                 }
                 else{

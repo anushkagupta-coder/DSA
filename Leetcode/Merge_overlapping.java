f7c4e8a750d5bdca2cf941b66861b6b9fadf83d4
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-10 20:30:23 +0530

    Update Merge_overlapping.java

--- Leetcode/Merge_overlapping.java
+++ Leetcode/Merge_overlapping.java
@@ -2,7 +2,7 @@ import java.util.*; // Needed for Arrays and List classes
 
 public class Merge_overlapping {
     public static void main(String[] args) {
-        int[][] arr = {{1, 2}, {1, 3}, {5, 6}, {4, 7}}; // ✅ Correct way to initialize a 2D array
+        int[][] arr = {{1, 2}, {1, 3}, {5, 6}, {4, 7}}; 
         int n = arr.length;
 
         int[][] merged = merge(arr, n); // Call the merge function

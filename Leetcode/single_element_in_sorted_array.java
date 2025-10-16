17988d8e54c18be496a97ce0825a26d9e362cf44
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-16 23:40:14 +0530

    Update single_element_in_sorted_array.java

--- Leetcode/single_element_in_sorted_array.java
+++ Leetcode/single_element_in_sorted_array.java
@@ -1,5 +1,11 @@
 
 //this code is to find out the only single element that is occuring singly in the sortred array baaki sab occuring twice 
 public class single_element_in_sorted_array {
-    
+    public static void main(String[] args) {
+        
+    }
+
+    static void func (int[] arr, int n){
+
+    }
 }

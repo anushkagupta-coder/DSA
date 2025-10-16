203c0f5ee054995d520e61f2604bb4fdf5ad3c27
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-17 00:27:36 +0530

    Update peak_element.java

--- Leetcode/peak_element.java
+++ Leetcode/peak_element.java
@@ -13,10 +13,15 @@ public class peak_element {
     static int func(INt[] srr,int n){
         int low=0;
         int high=n-1;
+        while(low<high){
+            int mid=(low+high)/2;
 
-        if(mid%2==0 && arr[mid]>arr[mid+1])[
+            if(arr[mid]>arr[mid+1]){
 
-        ]
+            }
+
+        }
         
+
     }
 }

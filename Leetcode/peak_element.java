31755a73105d8e0781304893d360e7aa0bc50b95
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-17 00:21:48 +0530

    Update peak_element.java

--- Leetcode/peak_element.java
+++ Leetcode/peak_element.java
@@ -1,9 +1,12 @@
+
+//this code id to find peaks . peaks ka malab is like having a graph having peak matlab ek element chota phir bada phir usse chota like 1,3,2 3 is at opeak
+
 public class peak_element {
     public static void main(String[] args) {
         
     }
 
     static int func({
-        
+
     })
 }

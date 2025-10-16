087bbf9c208c8222fca4f499091f2c75e726a53f
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-17 00:33:25 +0530

    Update peak_element.java

--- Leetcode/peak_element.java
+++ Leetcode/peak_element.java
@@ -3,8 +3,8 @@
 
 public class peak_element {
     public static void main(String[] args) {
-        int[] arr={};
-        int n=arr.length-1;
+        int[] arr={1, 2, 1, 3, 5, 6, 4};
+        int n=arr.length;
 
         int ans=func(arr,n);
         System.out.println(ans);
@@ -22,8 +22,9 @@ public class peak_element {
             else{
                 low=mid+1;
             }
-            return low;
+           
         }
+         return low;
         
 
     }

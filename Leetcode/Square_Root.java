60f95abd5195ec5237c2d415bb5960a3b3ebcdb1
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-17 18:34:24 +0530

    Update Square_Root.java

--- Leetcode/Square_Root.java
+++ Leetcode/Square_Root.java
@@ -1,4 +1,31 @@
 //finding square root of a number using binary search
+import java.util.Scanner;
+
 public class Square_Root {
-    
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(system.in);
+        System.out.println("Enter the Number");
+        int n = scanner.nextInt();
+        int ans=func(n);
+        System.out.println("Square root of the number" + n+ "is" +ans);
+    }
+    static void func(int arr[] ,int n){
+        int low=1;
+        int high=n;
+        while(low < high){
+            int mid=(low+high)/2 ;
+            int val=mid*mid;
+
+            if(val<=n){
+                low=mid+1;
+
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high ;
+
+    }
 }

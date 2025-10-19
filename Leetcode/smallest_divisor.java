ed4c3487d2ea6baedb584e518527057e5e26fa8a
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-19 19:53:43 +0530

    Create smallest_divisor.java

---
+++ Leetcode/smallest_divisor.java
@@ -0,0 +1,36 @@
+public class smallest_divisor {
+    public static void main(String[] args) {
+        
+    }
+
+    static int sumbyd(int[] arr, int div){
+        int sum=0;
+        for (int num : arr) {
+            sum += Math.ceil((double) num / div);
+        }
+        return sum;
+    }
+
+    static int func(int[] nums,int threshold){
+        int low=1;
+        int high=0;
+        for (int num : nums) {
+            high = Math.max(high, num);
+        }
+        int ans=-1;
+        while (low<=high){
+            int mid=(low+high)/2;
+            int sum=sumbyd(nums, mid);
+
+            if(sum<=threshold){
+                ans=mid;
+                high=mid+1;
+
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}

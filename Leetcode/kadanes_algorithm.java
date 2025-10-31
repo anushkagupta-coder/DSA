01b497f763c1b1ca58165b09f2d9112ec8f31bb2
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-31 17:59:34 +0530

    Create kadanes_algorithm.java

---
+++ Leetcode/kadanes_algorithm.java
@@ -0,0 +1,7 @@
+//this code is to find max sum of the subarrays and we are usingkadanes algorithm here
+//what is kadanes it basically means we have a count for sum and sum 
+//whenever the sum is less than 0 we reset and it and when sumcnt is greater than the sum then we keep the max cnt im mind  
+
+public class kadanes_algorithm {
+    
+}

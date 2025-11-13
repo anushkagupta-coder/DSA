62050d91e0436e5d3e1239bf170ff342758eee8c
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-11-13 13:18:31 +0530

    Create Java_collection.java

---
+++ Leetcode/Java_collection.java
@@ -0,0 +1,16 @@
+//Here i am writting lal the java collection and learning them 
+//List , map hashmap , set 
+//List - Ordered collection, allows duplicates
+
+Dynamic size (unlike arrays)
+
+import java.util.ArrayList;
+
+public class Java_collection {
+    public static void main(String[] args) {
+    // to initialize list 
+    ArrayList<> Arraylist = new ArrayList<>();     
+    }
+
+
+}

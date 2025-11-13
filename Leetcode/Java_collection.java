620b67ac6894da2b370363bffb25de04b88e3be5
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-11-13 13:34:58 +0530

    Update Java_collection.java

--- Leetcode/Java_collection.java
+++ Leetcode/Java_collection.java
@@ -1,10 +1,9 @@
 //Here i am writting lal the java collection and learning them 
 //List , map hashmap , set 
+
 //List - Ordered collection, allows duplicates Dynamic size (unlike arrays)
 
-
-
-import java.util.ArrayList;
+/*import java.util.ArrayList;
 
 public class Java_collection {
     public static void main(String[] args) {
@@ -14,7 +13,38 @@ public class Java_collection {
     fruits.add("Apple");
     fruits.add("Mango");
     fruits.add("MEEE");
+
+    System.out.println(fruits.get(2));
+
+    fruits.remove("Mango");
+
+    for(String f : fruits){
+        System.out.println(f);
     }
+    System.out.println("total frfuits:" + fruits.size());
+    }
+    
 
+}*/
 
+//------------------------------------------------------------//
+//Set Hashset - Unordered collection, no duplicates 
+//Useful when you want unique elements only
+
+/*import java.util.HashSet;
+
+public class Java_collection{
+    public static void main(String[] args) {
+        HashSet<String> fruits = new HashSet<>();
+         fruits.add("apple");
+         fruits.add("mango");
+
+         for(String f: fruits){
+            System.out.println(f);
+         }
+         System.out.println(fruits.size());
+
+    }
 }
+
+

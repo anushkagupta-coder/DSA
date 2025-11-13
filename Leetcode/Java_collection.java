705bce79f6e090fb973065e795d64aae047b5f20
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-11-13 13:23:02 +0530

    Update Java_collection.java

--- Leetcode/Java_collection.java
+++ Leetcode/Java_collection.java
@@ -1,15 +1,19 @@
 //Here i am writting lal the java collection and learning them 
 //List , map hashmap , set 
-//List - Ordered collection, allows duplicates
+//List - Ordered collection, allows duplicates Dynamic size (unlike arrays)
 
-Dynamic size (unlike arrays)
+
 
 import java.util.ArrayList;
 
 public class Java_collection {
     public static void main(String[] args) {
     // to initialize list 
-    ArrayList<> Arraylist = new ArrayList<>();     
+    ArrayList<String> fruits = new ArrayList<>();    
+    
+    fruits.add("Apple");
+    fruits.add("Mango");
+    fruits.add("MEEE");
     }
 
 

c69d01bc6600e8738f19e844752e4c6c279aa2c2
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-11-14 18:04:01 +0530

    Create Insert_DLL.java

---
+++ Leetcode/Insert_DLL.java
@@ -0,0 +1,47 @@
+//this code is to practise insertion of the data in Doubly linked list'
+//we will have to pointer back and next 
+//1.insertion at beggining 
+//2.insertion at end 
+//3.insertion at kth position 
+
+class node{
+    int data;
+    node next;
+    node back;
+    node(int data){
+        this.data=data;
+        this.next=null;
+        this.back=null;
+    }
+}
+
+public class Insert_DLL {
+    public static void main(String[] args) {
+        static node head = null;
+        head = new node(10);
+        head=insertNode(30);
+
+    }
+
+    static node insertNode(int data,int prev , int next){
+        node newNode = new node(data);
+
+        if(head == null){
+            head = newNode;
+            return head;
+        }
+        newNode.next = head;
+        head.back=newNode;
+        head=newNode;
+    }
+
+    static void printDLL(node head){
+        node temp=head;
+        while(temp!= null){
+            System.out.println(temp.data + " <-> ");
+            temp=temp.next;
+        }
+        System.out.println("Null");
+    }
+
+}

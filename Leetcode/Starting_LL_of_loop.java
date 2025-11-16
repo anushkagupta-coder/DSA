f8f51827d8f04827cf43a6076a6e22bc4b2e6b26
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-11-16 10:59:23 +0530

    Update Starting_LL_of_loop.java

--- Leetcode/Starting_LL_of_loop.java
+++ Leetcode/Starting_LL_of_loop.java
@@ -14,8 +14,12 @@
     }
  }
 public class Starting_LL_of_loop {
+    static Node head=null;
     public static void main(String[] args) {
-        
+        Node head= new Node(10);
+        head.next=new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = head.next;
     }
     static Node Starting(Node head){
         //1.find if loop exist;

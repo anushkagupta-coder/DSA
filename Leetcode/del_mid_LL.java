be53afcf1333f2a694294871a323382e44d140f9
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-11-16 12:39:26 +0530

    Update del_mid_LL.java

--- Leetcode/del_mid_LL.java
+++ Leetcode/del_mid_LL.java
@@ -1,14 +1,14 @@
-//program to delete middle node of LL
+//program to delete middle Node of LL
 /*
  *find mid using hare and tortoise when loop ends slow is at mid
  del slow using prev.next=slow.next
  */
 
-class node{
+class Node{
     int data;
-    node next;
+    Node next;
 
-    node(int data){
+    Node(int data){
         this.data=data;
         this.next=null;
 
@@ -17,18 +17,33 @@ class node{
 
 public class del_mid_LL {
     public static void main(String[] args) {
-        
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        System.out.println("list");
+        System.out.println(PrintNode(head));
+        System.out.println("after deletion");
+        Node head=del(head);
+        System.out.println(PrintNode(head));
     }
-    static node del(node head){
-        node slow=head;
-        node fast=head;
+    static Node del(Node head){
+        Node slow=head;
+        Node fast=head;
 
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
-        node prev;
+        Node prev;
         prev.next=slow.next;
-        
+
+    }
+    static Node PrintNode(node head){
+                Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
     }
 }

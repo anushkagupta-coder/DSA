0e382e8bf0809550ea8efa14961b92045526e377
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-11-23 19:17:42 +0530

    reverse number

---
+++ Leetcode/Reverse_Number.java
@@ -0,0 +1,20 @@
+//this code is to reverse the number 
+//123==321
+import java.util.Scanner;
+
+public class Reverse_Number {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the number to reverse");
+        int n=scanner.nextInt();
+        fun(n);
+
+    }
+
+    static void fun(int n){
+        //n=n%10;
+        //n=n/10;
+        
+        //reversed = reversed *10 +last digit;
+    }
+}

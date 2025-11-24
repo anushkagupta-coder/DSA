56408869d9313da7435b8804161d3bb15373962d
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-11-24 16:26:25 +0530

    Update pattern6.java

--- Pattern.java/pattern6.java
+++ Pattern.java/pattern6.java
@@ -1,8 +1,8 @@
 public class pattern6{
     public static void main(String[] args) {
-        for (int i = 5; i < 0; i++){
-            for(int j=i ;j<i+1;j++){
-                System.out.println(i);
+        for (int i = 5; i >=1; i++){
+            for(int j=1 ;j<i;j++){
+                System.out.println(j);
             }
             System.out.println( );
             

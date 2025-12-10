9286106f0497ec0999b43817331f2b95c56ec964
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-10 22:46:04 +0530

    Update Maximum_water.java

--- Leetcode/Maximum_water.java
+++ Leetcode/Maximum_water.java
@@ -1,23 +1,21 @@
-import java.util.*;
-//this program is to find the maximum water cna be hold or the area kind of 
 //here the trick is that the minimu heigth will decide this 
 public class Maximum_water {
     public static void main(String[] args) {
         int[] arr={2,4,6,8,3,4,5};
         int n=arr.length;
         System.out.println("maximum water");
-        System.out.println(func(arr));
+        System.out.println(func(arr , n));
     }
     static int func(int[] arr,int n ){
-        int height ;
-        int left,right ;
-        left=0;
-        right=n-1;
+        int left=0 ;
+        int right=n-1 ;
         int max_water=0;
-        int height=Math.min(arr[left],arr[right]);
-        int width=(right-left);
+        
      
         while(left<right){
+            
+            int height=Math.min(arr[left],arr[right]);
+            int width=(right-left);
             int water=width*height;
             if(water>max_water){
                 max_water=water;
@@ -27,7 +25,7 @@ public class Maximum_water {
                 left++;
             }
             else{
-                right++;
+                right--;
             }
         }
 return max_water;

e9b3ae83ac634e9f7b1fc9347378cfac31140a5b
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-11 00:06:04 +0530

    Create Remove_outermost_parenthesis.java

---
+++ Leetcode/Remove_outermost_parenthesis.java
@@ -0,0 +1,27 @@
+public class Remove_outermost_parenthesis {
+    public static void main(String[] args){
+
+
+
+    static int fun(){
+    StringBuilder result = new StringBuilder();
+        int depth = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (depth > 0) {
+                    result.append(c);
+                }
+                depth++;
+            } else { // c == ')'
+                depth--;
+                if (depth > 0) {
+                    result.append(c);
+                }
+            }
+        }
+
+        return result.toString();
+    }
+    }
+}

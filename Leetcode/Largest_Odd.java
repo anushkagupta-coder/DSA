1f3da7faa4dc9386c023f89bc2d6e7d9ca73f28a
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-11 22:47:04 +0530

    Update Largest_Odd.java

--- Leetcode/Largest_Odd.java
+++ Leetcode/Largest_Odd.java
@@ -1,17 +1,17 @@
 public class Largest_Odd {
     public static void main(String[] args) {
-        String str = 1234589;
+        String str = "1234589";
         System.out.println("the larget odd number is");
-        System.out.println(str);
+        System.out.println(fun(str));
     }
 
-    static int fun(String str){
+    static String fun(String str){
         //left to right
         int n=str.length();
         for(int i=n-1; i>=0;i--){
             int digit=str.charAt(i) - '0'; 
 
-            if(digit%2==0){
+            if(digit%2==1){
                 return str.substring(0,i+1);
             }
         }

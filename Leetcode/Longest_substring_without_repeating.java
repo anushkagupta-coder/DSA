6036d27bbc30f462a8b1296a1b1c54f0e7072057
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-23 16:39:00 +0530

    Update Longest_substring_without_repeating.java

---
+++ Leetcode/Longest_substring_without_repeating.java
@@ -0,0 +1,36 @@
+//this is a sliding window probem here we will be using teo pointers right and left 
+//add into set or hashmap if not presnt if duplicate remove and move right count the total length;
+import java.util.Set;
+public class Longest_substring_without_repeating {
+    public static void main(String[] args){
+        string s=abcabad;
+        System.out.println(fun(s));
+    }
+    static int fun(String s){
+        int right=0;
+        int left=0;
+        int maxlength=0;
+        Set<Character> set =new HashSet<>();
+
+        while(right < s.length()){
+           char currentchar = s.charAt(right);
+
+           if(!set.contains(currentchar)){
+            set.add(currentchar);
+            maxlength=Math.max(maxlength , right-left+1);
+            right++;
+           }
+
+           else{
+            set.remove(s.charAt(left));
+            left++;
+           }
+
+            }
+
+            return maxlength;
+
+        }
+
+    }
+

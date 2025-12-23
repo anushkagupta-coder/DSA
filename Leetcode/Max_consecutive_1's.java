b59295e1155f9c7fb1285571414b3e0a7f87596c
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-23 17:11:29 +0530

    Update Max_consecutive_1's.java

--- Leetcode/Max_consecutive_1's.java
+++ Leetcode/Max_consecutive_1's.java
@@ -1,6 +1,6 @@
 public class Max_consecutive_1's {
     public static void main(String[] args){
-        int[] nums= [1,1,1,0,0,0,1,1,1,1,0];
+        int[] nums= {1,1,1,0,0,0,1,1,1,1,0};
         int k=2;
         System.out.println("Answer");
         System.out.println(fun(nums, k));

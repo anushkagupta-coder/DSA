d215ec1e0a712eb95707e291e718fadb9ce17a12
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-24 09:31:08 +0530

    Create TrieTree.java

---
+++ Leetcode/TrieTree.java
@@ -0,0 +1,26 @@
+class TrieNode{
+    TrieNode[] children;
+    boolean isEndOfWord;
+
+    TrieNode(){
+        children = new TrieNode[26];
+        isEndOfWord=false;
+    }
+}
+
+class Trie{
+    TrieNode root;
+
+    Trie(){
+        root = new TrieNode();
+    }
+}
+
+public class TrieTree {
+    public static void main(String[] args) {
+        
+    }
+
+    static void insert(String word)
+}
+

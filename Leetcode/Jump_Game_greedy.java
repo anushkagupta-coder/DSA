32a07251db2d65dfa0ce7650b0950f0254b27c7f
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-25 01:21:17 +0530

    Update Jump_Game_greedy.java

--- Leetcode/Jump_Game_greedy.java
+++ Leetcode/Jump_Game_greedy.java
@@ -3,6 +3,8 @@ public class Jump_Game_greedy {
         int[] nums={1,3,4,1,0,4,2};
         System.out.println("answer");
         System.out.println(canJump(nums));
+        System.out.println("if its true how many jumps will it require ");
+        System.out.println(Countofjumps(nums));
     }
 
     static boolean canJump(int[] nums){
@@ -18,5 +20,25 @@ public class Jump_Game_greedy {
         }
 
         return true;
+    }
+
+    //Now to find ki pata hai can reach end 
+    //find kitne jumps will be required
+
+    static int Countofjumps(int[] nums){
+        int currentend=0;
+        int maxreach=0;
+        int jumps = 0;
+
+        for(int i=0; i< nums.length-1 ;i++){
+             farthest = Math.max(farthest, i + nums[i]);
+
+             if(i==currentend){
+                jumps++;
+                currentend=farthest;
+
+             }
+        }
+        return jumps;
     }
 }

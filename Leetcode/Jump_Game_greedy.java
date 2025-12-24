84984a4704d31c6c4aa464d6a26997ef81360449
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-25 00:33:12 +0530

    Update Jump_Game_greedy.java

---
+++ Leetcode/Jump_Game_greedy.java
@@ -0,0 +1,22 @@
+public class Jump_Game_greedy {
+    public static void main(String[] args) {
+        int[] nums={1,3,4,1,0,4,2};
+        System.out.println("answer");
+        System.out.println(canJump(nums));
+    }
+
+    static void canJump(int[] nums){
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > maxReach) {
+                return false;
+            }
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+}

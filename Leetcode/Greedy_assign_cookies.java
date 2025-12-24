41f7757e3827b0c0ecad8128e9a54d1d070656c3
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-24 15:33:29 +0530

    Update Greedy_assign_cookies.java

--- Leetcode/Greedy_assign_cookies.java
+++ Leetcode/Greedy_assign_cookies.java
@@ -1,22 +1,29 @@
+import java.util.Arrays;
+//sabse pehle isme sort karo aarays
+//phir check kro using left and right pointers 
 public class Greedy_assign_cookies {
     public static void main(String[] args){
+        int[] greed = {1,2,3};
+        int[] size = {1,1}; 
 
+        System.out.println("answer");
+        System.out.println(func(greed, size));
     }
 
-    static int func(int[] greedy , int[] size){
-        int a=greedy.length;
+    static int func(int[] greed , int[] size){
+        int a=greed.length;
         int b= size.length;
 
         int left=0;
         int right=0;
         Arrays.sort(greed);
         Arrays.sort(size);
-        while(left<b){
-            if(greed[r]<=size[l]){
+        while(left < a && right < b){
+            if(size[right] >= greed[left]){
                 right=right+1;
             }
             left=left+1;
         }
-        return r;
+        return right;
     }
 }

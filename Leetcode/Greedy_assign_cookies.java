ca1cf04d01905ff2c753ac85885d69eeba303c2d
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-24 15:12:59 +0530

    Update Greedy_assign_cookies.java

--- Leetcode/Greedy_assign_cookies.java
+++ Leetcode/Greedy_assign_cookies.java
@@ -3,19 +3,19 @@ public class Greedy_assign_cookies {
 
     }
 
-    static void func(int[] greedy , int[] size){
+    static int func(int[] greedy , int[] size){
         int a=greedy.length;
         int b= size.length;
 
         int left=0;
         int right=0;
-        sort(greed);
-        sort(size);
+        Arrays.sort(greed);
+        Arrays.sort(size);
         while(left<b){
             if(greed[r]<=size[l]){
-                r=r+1;
+                right=right+1;
             }
-            l=l+1;
+            left=left+1;
         }
         return r;
     }

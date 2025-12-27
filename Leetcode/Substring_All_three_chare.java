5a4dcbb0cbbd02485f10d444749487aaf63f4bc7
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-27 11:06:19 +0530

    Create Substring_All_three_chare.java

---
+++ Leetcode/Substring_All_three_chare.java
@@ -0,0 +1,5 @@
+//find a substring having all 3 charecters 
+//ek baar ek mili toh use right ke sab parts bhihonge hi substring having all 3 charecters
+public class Substring_All_three_chare {
+    
+}

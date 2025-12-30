4daa5068480c26f59b06f6bbf39dcc2a51959bda
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-30 13:45:59 +0530

    Update Reverse_word_in_String.java

--- Leetcode/Reverse_word_in_String.java
+++ Leetcode/Reverse_word_in_String.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Reverse_word_in_String {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -7,16 +8,16 @@ public class Reverse_word_in_String {
     }
 
     static String fun(String s){
-        String words=s.trim.split("\\s+");
+        String[] words=s.trim().split("\\s+");
 
         StringBuilder result = new StringBuilder();
 
         for(int i=words.length-1 ; i>=0 ; i--){
             result.append(words[i]);
-            if(i!+0){
+            if(i!=0){
                 result.append(" ");
             }
         }
-        return resu;t.toString();
+        return result.toString();
     }
 }

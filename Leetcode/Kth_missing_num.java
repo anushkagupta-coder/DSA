4c65abd0f852b8d35e3e1783b0cef91404760d95
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-31 17:22:40 +0530

    Update Kth_missing_num.java

--- Leetcode/Kth_missing_num.java
+++ Leetcode/Kth_missing_num.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 public class Kth_missing_num {
     public static void main(String[] args){
         Scanner scanner=new  Scanner(System.in);
-        System.out.println("enter the array");
-        int[] arr= new int[10];
-        System.out.println("enter size of the array");
+       
+          System.out.println("enter size of the array");
         int n= scanner.nextInt();
+        int[] arr= new int[n];
+       System.out.println("enter the array");
+        
         for(int i=0;i<n;i++){
             arr[i]=scanner.nextInt();
         }
@@ -24,7 +26,7 @@ public class Kth_missing_num {
         for(int i=0 ; i<n ;i++){
             int missingcount=arr[i]-(i+1);
             if (missingcount>=k){
-                return k+1;
+                return k+i;
             }
         }
         return k+n;

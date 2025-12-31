5abc31c5cc8e95dbcb3f621919a2ad014cc00891
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-12-31 11:30:15 +0530

    Create Kth_missing_num.java

---
+++ Leetcode/Kth_missing_num.java
@@ -0,0 +1,33 @@
+//this code is to find the kth missing number from set of natural numbers 
+//imp formalus are the key u have to keep them in mind no matter how 
+
+
+import java.util.Scanner;
+public class Kth_missing_num {
+    public static void main(String[] args){
+        Scanner scanner=new  Scanner(System.in);
+        System.out.println("enter the array");
+        int[] arr= new int[10];
+        System.out.println("enter size of the array");
+        int n= scanner.nextInt();
+        for(int i=0;i<n;i++){
+            arr[i]=scanner.nextInt();
+        }
+        System.out.println("which kth missing number u have to find");
+        int k=scanner.nextInt();
+
+        System.out.println("the kth missing number is"+fun(arr,k));
+    }
+
+    static int fun(int[] arr,int k){
+        int n=arr.length;
+        for(int i=0 ; i<n ;i++){
+            int missingcount=arr[i]-(i+1);
+            if (missingcount>=k){
+                return k+1;
+            }
+        }
+        return k+n;
+    }
+    
+}

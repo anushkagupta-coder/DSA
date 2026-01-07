36cc850c5c3b2caba77f8c2cdd5c12129d21647a
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2026-01-08 01:18:45 +0530

    right view of binary tree

---
+++ Leetcode/RightSideViewBinaryTree.java
@@ -0,0 +1,47 @@
+//bfs kro ,level kaa last element yaad rkho
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) { this.val = val; }
+}
+
+public class RightSideViewBinaryTree {
+
+    public static List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+
+            for(int i=0; i<size; i++) {
+                TreeNode curr = q.poll();
+
+                // last node of this level
+                if(i == size-1) result.add(curr.val);
+
+                if(curr.left != null) q.offer(curr.left);
+                if(curr.right != null) q.offer(curr.right);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+
+        System.out.println(rightSideView(root)); // [1,3,6]
+    }
+}

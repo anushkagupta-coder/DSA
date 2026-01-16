09a1892977f9b12d6cbda36240276ba7314ae5ce
“Anushka <anushka612004@gmail.com>
2026-01-16 12:15:44 +0530

    trying gitbash

--- Leetcode/Balanced_binary_tree.java
+++ Leetcode/Balanced_binary_tree.java
@@ -11,6 +11,7 @@ class TreeNode{
 public class Balanced_binary_tree {
     public static void main(String[] args) {
         
+        
      TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
